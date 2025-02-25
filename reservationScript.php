c206b00a04a6a931bf74e8f63cc1a6c4c8303d27
ju <2120465@ub.edu.ph>
2025-02-25 09:28:38 +0800

    Update reservationScript.php

--- reservationScript.php
+++ reservationScript.php
@@ -10,36 +10,37 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 try{
 	$connection = mysqli_connect($server, $user, $pass, $db);
-}catch (Exception $ex)
+}
+
+catch (Exception $ex)
 {
 	echo'Error';
 }
 
 if(isset($_POST['Reservation']))
 {
+
 	$Full_Name = mysqli_real_escape_string($connection, $_POST['Full_Name']);
-        $Date = mysqli_real_escape_string($connection, $_POST['Date']);
-        $Time = mysqli_real_escape_string($connection, $_POST['Time']);
+    	$Date = mysqli_real_escape_string($connection, $_POST['Date']);
+    	$Time = mysqli_real_escape_string($connection, $_POST['Time']);
 	$Email = mysqli_real_escape_string($connection, $_POST['Email']);
 	$Pax = mysqli_real_escape_string($connection, $_POST['Pax']);
-	
+
+	//insert data to table
 	$insert = "INSERT INTO tablereservation_tbl (Full_Name, Reservation_Date, Reservation_Time, Email, Pax, Reservation_Created, Status)
-		   VALUES('$Full_Name', '$Date', '$Time', '$Email', '$Pax', NOW(), 'confirmed')";  
+			   VALUES('$Full_Name', '$Date', '$Time', '$Email', '$Pax', NOW(), 'confirmed')";  
 
-	try{
-		$insert_result = mysqli_query($connection, $insert);
-		
-		if($insert)
-		{
-			if(mysqli_affected_rows($connection) > 0)
-			{
-				print '<script>alert("Sucessfully Reserved!");</script>';
-				Print '<script>window.location.assign("menu.html");</script>';
-
-			}
-		catch (Exception $ex)
-		{
-		echo 'Error Insert'.$ex -> getMessage();
-		}
-	}
+try{
+    $insert_result = mysqli_query($connection, $insert);
+    
+    if (!$insert_result) {
+        throw new Exception('Insert Query Error in patient_info: ' . mysqli_error($connection));
+    }
+        echo '<script>alert("Successfully Registered!");</script>';
+        echo '<script>window.location.assign("reservation.html");</script>';
+    }   
+catch (Exception $ex) {
+    echo 'Error: ' . $ex->getMessage();
+    }
+}
 ?>
