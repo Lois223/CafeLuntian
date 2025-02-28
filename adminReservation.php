dc31be67627a423655c207864dd4d9fc87c83b2c
ju <2120465@ub.edu.ph>
2025-02-28 12:55:17 +0800

    Add files via upload

--- adminReservation.php
+++ adminReservation.php
@@ -52,7 +52,7 @@
         <div class="user-profile">
           <img src="img/profile.png" alt="Profile Image" />
           <div class="user-detail">
-            <h3><?php echo $_SESSION['FName']." ".$_SESSION['LName']; ?></h3>
+            <h3><?php echo $_SESSION['User_FName']." ".$_SESSION['User_LName']; ?></h3>
             <span>Admin</span>
           </div>
         </div>
@@ -60,11 +60,87 @@
     </aside>
 
     <main class="main">
-      <header>
-        <h1>Reservation</h1>
-      </header>
+        <header>
+            <h1>Reservation List</h1>
+        </header>
 
-    
+        <section class="search">
+            <a href="#" class="search-icon"><i class="fas fa-search"></i></a>
+            <input type="text" id="searchInput" placeholder="Search Name or ID here" >   
+        </section>
+
+        <section class="search-result">
+            <h1>Search Results</h1><hr>
+            <div class="box-container" id="reservationList">
+                <!-- PHP START HERE -->
+                <?php
+                include('mycon.php');
+                $sql = "SELECT * FROM table_reservation_tbl";
+                $result = $connection->query($sql);
+                echo "<table id='userTable' border='1' width='100%' id='userTable'>";
+                echo "<tr align='center' class=tblheader>
+                        <td><b>Reservation_ID</b></td>
+                        <td><b>Full Name</b></td>
+                        <td><b>Reservation_Date</b></td>
+                        <td><b>Reservation_Time</b></td>
+                        <td><b>Email</b></td>
+                        <td><b>Number of Guests</b></td>
+                        <td><b>Reservation Created</b></td>
+                        <td><b>Status</b></td>
+                      </tr>";
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $Reservation_ID = $row['Reservation_ID'];
+                        echo'<tr>
+                                <td class="user-id">'.$row['Reservation_ID'].'</td>
+                                <td class="user-name">'.$row['Full_Name'].'</td>
+                                <td>'.$row['Reservation_Date'].'</td>  
+                                <td>'.$row['Reservation_Time'].'</td>
+                                <td>'.$row['Email'].'</td>    
+                                <td>'.$row['Pax'].'</td>   
+                                <td>'.$row['Reservation_Created'].'</td>   
+                                <td>'.$row['Status'].'</td>   
+                                <td>
+                                  <a class="remove-row-button" href="DeletionQueries.php?act=DeleteUser&Reservation_ID=' . urlencode($row['Reservation_ID']) . '" onclick="return confirm(\'Are you sure you want to delete this account?\');" class="icon-button">
+                                      <i class="fas fa-trash-alt"></i>
+                                  </a>
+                                </td>                             
+                            </tr>';
+                    }
+                } else {
+                     echo "<tr><td colspan='6' align='center'>No results found.</td></tr>";
+                  }
+                echo "</table>";
+              ?>
+            </div>
+        </section>
     </main>
-  </body>
+
+    <script src="main.js"></script>
+    <script>
+      //search bar input
+      const searchInput = document.getElementById("searchInput");
+      const patientTable = document.getElementById("userTable");
+      const tableRows = patientTable.getElementsByTagName("tr");
+
+      // Listen for input events 
+      searchInput.addEventListener("input", function () {
+        const searchTerm = searchInput.value.toLowerCase();
+
+        // Loop through all rows in the table 
+        for (let i = 1; i < tableRows.length; i++) {
+            const row = tableRows[i];
+            const userID = row.getElementsByClassName("user-id")[0].innerText.toLowerCase();
+            const userName = row.getElementsByClassName("user-name")[0].innerText.toLowerCase();
+            const userPosition = row.getElementsByClassName("user-position")[0].innerText.toLowerCase();
+
+            // Check if the search term is in the User ID, Name, Position
+            if (userID.includes(searchTerm) || userName.includes(searchTerm) || userPosition.includes(searchTerm)) {
+                row.style.display = ""; // Show row if it matches
+            } else {
+                row.style.display = "none"; // Hide row if it doesn't match
+            }
+        }
+      });
+    </script>
 </html>
